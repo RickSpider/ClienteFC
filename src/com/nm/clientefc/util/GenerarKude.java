4cfc60a591e8587e6e51994046ad9232a647e207
RickSpider <rrgi89@hotmail.com>
2022-12-26 13:12:39 -0300

    Clase Generar KUDE

---
+++ src/com/nm/clientefc/util/GenerarKude.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nm.clientefc.util;
+
+import com.nm.clientefc.modelo.Comprobante;
+
+/**
+ *
+ * @author BlackSpider
+ */
+public class GenerarKude {
+    
+    private String pathReporte;
+    private Comprobante comprobante;
+    private String pathSalida;
+    
+    public void generarArchivoPDF(){
+    
+        
+    
+    }
+    
+}
